8233cdbf5485243fdbe37b4b904aff7da1a14a60
indrjeetsinghsaini <121189255+indrjeetsinghsaini@users.noreply.github.com>
2025-10-08 02:05:53 -0700

    Refactor context providers and add StrictMode
    Rearranged context providers and added StrictMode for better rendering.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,22 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import UserContext from './context/UserContext.jsx';
-import CaptainContext from './context/CapatainContext.jsx';
-import SocketProvider from './context/SocketContext.jsx';
+import CaptainContext from './context/CaptainContext.jsx'; // Corrected the typo
+import { SocketProvider } from './context/SocketContext.jsx'; // Added curly braces {}
 
 createRoot(document.getElementById('root')).render(
-
-  <CaptainContext>
-    <UserContext>
-      <SocketProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </SocketProvider>
-    </UserContext>
-  </CaptainContext>
-
-)
+  <React.StrictMode>
+    <BrowserRouter>
+      <UserContext>
+        <CaptainContext>
+          <SocketProvider>
+            <App />
+          </SocketProvider>
+        </CaptainContext>
+      </UserContext>
+    </BrowserRouter>
+  </React.StrictMode>
+);
